cf411e52b0bc244258f7dcfb0c0ef3b68bc00c48
masum691 <masumhu691@gmail.com>
2021-11-10 12:32:07 +0600

    products components bug fixed

--- src/Pages/Home/Products/Product/Product.js
+++ src/Pages/Home/Products/Product/Product.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Product = ({product}) => {
-    const {name} = product;
+    const {id, name, price, description, imgURL} = product;
     return (
         <div>
             <div>
                 <div class="col">
                     <div class="card h-100">
                         <img className="img-fluid service-img" src={imgURL} class="card-img-top" alt="..." />
-                        <div class="card-body">
+                        {/* <div class="card-body">
                             <div className="mb-2 card-icon-color">
                                 <span className="me-3"><i class="fas fa-record-vinyl"></i> {working}</span>
                                 <span><i class="fas fa-map-marker-alt"></i> {location}</span>
-                            </div>
+                            </div> */}
                             <h5 class="card-title">{name}</h5>
                             <p class="card-text">{description?.slice(0, 150)}</p>
                         </div>
@@ -23,14 +23,13 @@ const Product = ({product}) => {
                                     <span className="fs-5 fw-bold price">From $ {price}</span>
                                 </div>
                                 <div className="book-now">
-                                    <Link to={`/booking/${_id}`}><button>Book Now <i class="fas fa-cart-plus font-awesome"></i></button></Link>
+                                    <Link to={`/booking/${id}`}><button>Book Now <i class="fas fa-cart-plus font-awesome"></i></button></Link>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
--- src/Pages/Home/Products/Products/Products.js
+++ src/Pages/Home/Products/Products/Products.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 
 const Products = () => {
-    const [products, setProducts] = useState();
+    const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('')
-        .then(res => res.json)
-        .then(data => setProducts(data))
+        fetch('./products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, [])
     return (
-        <div>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 container mx-auto mb-5">
             {
-                products.map( product => <Product
+                products.map(product => <Product
                     product={product}
                 ></Product>)
             }
