0cfeeba076e97cb18cb4c403e23388232c5b2ae9
masum691 <masumhu691@gmail.com>
2021-11-10 21:32:14 +0600

    feedback components

---
+++ src/Pages/Home/Feedback/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Feedback.css'
+
+const Feedback = () => {
+    return (
+        <div className="text-center feedback">
+            <p>We’d love to hear what you think!</p>
+            <button>Give feedback</button>
+        </div>
+    );
+};
+
+export default Feedback;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import CustomerReviews from '../CustomerReviews/CustomerReviews';
 import Faq from '../Faq/Faq';
+import Feedback from '../Feedback/Feedback';
 import Products from '../Products/Products/Products';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Products></Products>
             <CustomerReviews></CustomerReviews>
             <Faq></Faq>
+            <Feedback></Feedback>
         </div>
     );
 };
