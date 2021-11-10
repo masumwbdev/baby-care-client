d2230464483af241158ede91c144f388f258f50e
masum691 <masumhu691@gmail.com>
2021-11-10 10:10:42 +0600

    notfound js

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Page not found</h1>
+        <div className="not-found text-center">
+            <i className="far fa-sad-cry notfound-emoji"></i>
+            <h1>404</h1>
+            <h5 className="mt-3">Page Not Found !!!</h5>
+            <button className="btn btn-primary btn-sm mt-3"><Link to="/home">Back Home</Link></button>
         </div>
     );
 };
